cddb9d5cd431e070041cc07b84cc69f9f4420bae
vuongng87 <vuongng.data@gmail.com>
2023-01-04 20:56:03 +0700

    add source

--- 2-examples/src/App.tsx
+++ 2-examples/src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import avatar from './assets/images/avatar.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <img src={avatar} alt="" />
+      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href={"https://google.com"} className="btn btn-primary">Go somewhere</a>
     </div>
+  </div>
   );
 }
 
